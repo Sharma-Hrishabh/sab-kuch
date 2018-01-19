97f977967038c8b36996d163428b1df8d0f54048
Sharma-Hrishabh <34400111+Sharma-Hrishabh@users.noreply.github.com>
2018-01-19 14:15:30 +0530

    login system added with register.php

---
+++ login-system/login-system/reset_password.php
@@ -0,0 +1,34 @@
+<?php
+/* passwrd reset process, updates database with new user password */
+require 'db.php';
+session_start();
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') { 
+
+    
+    if ( $_POST['newpassword'] == $_POST['confirmpassword'] ) { 
+
+        $new_password = password_hash($_POST['newpassword'], PASSWORD_BCRYPT);
+        
+        // we get $_POST['email'] and $_POST['hash'] from the hidden input field of reset.php form
+        $email = $mysqli->escape_string($_POST['email']);
+        $hash = $mysqli->escape_string($_POST['hash']);
+        
+        $sql = "UPDATE users SET password='$new_password', hash='$hash' WHERE email='$email'";
+
+        if ( $mysqli->query($sql) ) {
+
+        $_SESSION['message'] = "Your password has been reset successfully!";
+        header("location: success.php");    
+
+        }
+
+    }
+    else {
+        $_SESSION['message'] = "Two passwords you entered don't match, try again!";
+        header("location: error.php");    
+    }
+
+}
+?>
